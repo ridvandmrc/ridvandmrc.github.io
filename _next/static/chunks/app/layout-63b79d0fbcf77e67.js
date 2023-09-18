fce88b29f9af14431f4dd416d46005fe90817c9a
Rıdvan Demirci <ridvandemirci@192.168.1.36>
2023-09-19 00:48:39 +0300

    new version of portfolio

---
+++ _next/static/chunks/app/layout-63b79d0fbcf77e67.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{665:function(n,e,u){Promise.resolve().then(u.t.bind(u,536,23))},536:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=665)}),_N_E=n.O()}]);
